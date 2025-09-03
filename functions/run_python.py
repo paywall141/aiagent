f6eb067689b82cf5c73781e2c4433dddf1a9d386
Paywall141 <Paywall141@gmail.com>
2025-09-03 05:23:44 -0700

    added run_python working script and amnual tests pass

---
+++ functions/run_python.py
@@ -0,0 +1,36 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    try:
+        fullpath = os.path.abspath(os.path.join(working_directory,file_path ))
+        working_directory_abs = os.path.abspath(working_directory)
+
+        # validate path
+        if not fullpath.startswith(working_directory_abs):
+            return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+
+        # validate file_path exists
+        if not os.path.exists(fullpath):
+            return f'Error: File "{file_path}" not found.'
+        
+        if not fullpath.endswith('.py'):
+            return f'Error: "{file_path}" is not a Python file.'
+
+        if args is None:
+            args = []
+        # Build the command list
+        cmd = ["uv", "run", fullpath] + args
+        result = subprocess.run(cmd, capture_output=True, cwd=working_directory, timeout=30, text=True)
+        if result.returncode != 0:
+            return f"Process exited with code {result.returncode}"
+
+        output = result.stdout.strip() + result.stderr.strip()
+        if not output:
+            return "No output produced."
+
+        final_str = f"""STDOUT:\n{result.stdout}\nSTDERR:{result.stderr}"""        
+        return final_str
+
+    except Exception as e:
+        return f"Error: {e}" 
--- tests.py
+++ tests.py
@@ -1,19 +1,23 @@
 from functions.get_files_info import get_files_info
 from functions.get_file_content import get_file_content
 from functions.write_file import write_file
+from functions.run_python import run_python_file
 import os
+
+def test():
+    print("========TEST 1========")
+    print( run_python_file("calculator", "main.py") )
+    print("========TEST 2========")
+    print( run_python_file("calculator", "main.py", ["3 + 5"]) )
+    print("========TEST 3========")
+    print( run_python_file("calculator", "tests.py") )
+    print("========TEST 4========")
+    print( run_python_file("calculator", "../main.py") )
+    print("========TEST 4========")
+    print( run_python_file("calculator", "nonexistent.py") )
+
 
 if __name__ == "__main__":
     wd = os.getcwd()
-print("========TEST 1========")
-print( write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum") )
-print("========TEST 2========")
-print( write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet") )
-print("========TEST 3========")
-print( write_file("calculator", "/tmp/temp.txt", "this should not be allowed") )
-print("========TEST 4========")
-print("no test 4 to run")
-
-
-
-
+    test()
+    
