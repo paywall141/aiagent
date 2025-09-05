4fd36b06c22cab3662927ac298e0f5fa248b73bf
Paywall141 <Paywall141@gmail.com>
2025-09-05 11:06:42 -0700

    aligning github with new system_prompt

--- config.py
+++ config.py
@@ -10,5 +10,6 @@ When a user asks a question or makes a request, make a function call plan. You c
 - Write or overwrite files
 
 Your summaries should be concise and high-level. With numbered steps outlining your task surrounded with  ** followed by your result. then a final seperate line to cleanly read the summary 
+**!!Important!!** when fixing bugs, unless explicitly told to, DO NOT create new files. instead modify existing file code and report your changes so they can be reverted if deemed incorrect. 
 All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
 """
